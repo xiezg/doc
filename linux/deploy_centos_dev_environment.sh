fe3da40ae8c64731b5a5471972eea9c3b2b860e4
xiezg <xzghyd2008@hotmail.com>
2018-02-11 22:05:07 +0800

    添加部署CentOS开发环境的脚本

---
+++ linux/deploy_centos_dev_environment.sh
@@ -0,0 +1,13 @@
+#########################################################################
+# File Name: deploy_centos_dev_environment.sh
+# Author: xiezg
+# mail: xzghyd2008@hotmail.com
+# Created Time: 2018-02-11 17:15:11
+# Last modified: 2018-02-11 18:14:53
+#########################################################################
+#!/bin/bash
+
+yum -y install openssh-clients ctags man automake libtool
+
+
+
